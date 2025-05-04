2dff7b38146209e5581be8f6e231528e8d8355b4
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2025-05-04 18:44:44 -0500

    Added all categories and activities and items to admin now they can be added directly

--- weather4u/admin.py
+++ weather4u/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import ClothingItem, ClothingCategory
+from .models import ClothingItem, ClothingCategory, Activity, ActivityCategory
 
-admin.site.register(ClothingItem)
-admin.site.register(ClothingCategory)
+@admin.register(ClothingItem)
+class ClothingItemAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    filter_horizontal = ("categories",)
+
+@admin.register(ClothingCategory)
+class ClothingCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ("name", "min_temp", "max_temp")
+    search_fields = ("name",)
+    filter_horizontal = ("categories",)
+
+@admin.register(ActivityCategory)
+class ActivityCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
