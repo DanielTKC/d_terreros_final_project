58ad4432339fd94c7c1ab907743c708963241e86
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2025-05-04 08:50:25 -0500

    Pip install geoip2

--- weather4u/views.py
+++ weather4u/views.py
@@ -1,4 +1,9 @@
+import datetime
+import requests
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'weather4u/index.html')
+
+def my_weather(request):
+    current_weather_url =
